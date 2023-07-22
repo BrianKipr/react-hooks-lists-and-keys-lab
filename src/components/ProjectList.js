384e2aab5bcc0b475f46eb69e5a7b7aa50a81bbd
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-07-22 20:12:10 +0300

    assignment completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,3 +1,20 @@
+// import React from "react";
+// import ProjectItem from "./ProjectItem";
+
+// function ProjectList({ projects }) {
+//   console.log(projects);
+//   return (
+//     <div id="projects">
+//       <h2>My Projects</h2>
+//       <div id="project-list">{/* render ProjectItem components here */}</div>
+//     </div>
+//   );
+// }
+
+// export default ProjectList;
+
+
+
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
@@ -6,7 +23,16 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.name} // Assuming 'name' property is unique for each project
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
